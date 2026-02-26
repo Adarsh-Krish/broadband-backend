d0b858d5ddcf98917599edef926bd4413184970e
adarshKris <krishna.adarsh@careesteem.co.uk>
2026-02-26 11:45:59 +0000

    temp hardcode uri

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const uri =
+      process.env.MONGO_URI ||
+      "mongodb+srv://broadband_user:Tm84ITbv1e4QB2kz@broadband-cluster.wgq3i0z.mongodb.net/broadband?appName=broadband-cluster";
+    console.log("Connecting with URI:", uri ? "URI EXISTS" : "URI MISSING");
+    await mongoose.connect(uri);
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection failed:", err.message);
