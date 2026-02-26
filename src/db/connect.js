e340045d1f19a2bc3add61a3468c226ef1656d63
adarshKris <krishna.adarsh@careesteem.co.uk>
2026-02-26 11:50:16 +0000

    fix mongodb uri

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const uri =
-      process.env.MONGO_URI ||
-      "mongodb+srv://broadband_user:Tm84ITbv1e4QB2kz@broadband-cluster.wgq3i0z.mongodb.net/broadband?appName=broadband-cluster";
-    console.log("Connecting with URI:", uri ? "URI EXISTS" : "URI MISSING");
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      throw new Error("MONGO_URI environment variable is not set");
+    }
     await mongoose.connect(uri);
     console.log("✅ MongoDB connected");
   } catch (err) {
